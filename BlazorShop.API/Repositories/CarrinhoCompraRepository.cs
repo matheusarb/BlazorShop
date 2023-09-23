f6517fdeb969ffa61d0c007bdb898ed1a5b23d0f
Matheus <mat.araujoribeiro@gmail.com>
2023-09-23 19:54:38 -0300

    componentes e Interface do carrinho de compras

---
+++ BlazorShop.API/Repositories/CarrinhoCompraRepository.cs
@@ -0,0 +1,41 @@
+﻿using BlazorShop.Api.Context;
+using BlazorShop.API.Entities;
+using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.API.Repositories
+{
+    public class CarrinhoCompraRepository : ICarrinhoCompraRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CarrinhoCompraRepository(AppDbContext context)
+        {
+            _appDbContext = context;
+        }
+
+        public Task<CarrinhoItem> AdicionaItem(CarrinhoItemAdicionaDTO carrinhoItemAdicionaDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarrinhoItem> AtualizaQuantidade(int id, CarrinhoItemAtualizaQuantidadeDTO carrinhoItemAtualizaQuantidadeDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarrinhoItem> DeletaItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarrinhoItem> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CarrinhoItem>> GetItens(string usuarioId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorShop.API/Repositories/ICarrinhoCompraRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlazorShop.API.Entities;
+using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.API.Repositories
+{
+    public interface ICarrinhoCompraRepository
+    {
+        Task<CarrinhoItem> AdicionaItem(CarrinhoItemAdicionaDTO carrinhoItemAdicionaDTO);
+
+        Task<CarrinhoItem> AtualizaQuantidade(int id, CarrinhoItemAtualizaQuantidadeDTO carrinhoItemAtualizaQuantidadeDTO);
+
+        Task<CarrinhoItem> DeletaItem(int id);
+
+        Task<CarrinhoItem> GetItem(int id);
+
+        Task<IEnumerable<CarrinhoItem>> GetItens(string usuarioId);
+    }
+}
